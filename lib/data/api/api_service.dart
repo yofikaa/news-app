d228b5ae9061f66c2e24cd39a320b23d6bdaef73
yofikaa <yovictel@gmail.com>
2023-05-15 23:29:43 +0700

    6.6

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -3,14 +3,14 @@ import 'package:http/http.dart' as http;
 import 'package:news_app/data/model/article.dart';
 
 class ApiService{
-  static final String _baseUrl = 'https://newsapi.org/v2';
-  static final String _apiKey = 'YOUR_API_KEY';
-  static final String _category = 'business';
-  static final String _country = 'id';
+  static final String _baseUrl = 'http://newsapi.org/v2/';
+  static final String _apiKey = '9094ba21c74e472aabce0f3b7211d67a';
+  static final String _category = 'technology';
+  static final String _country = 'us';
 
 
   Future<ArticlesResult> topHeadlines() async {
-    final response = await http.get(Uri.parse(_baseUrl + "https://newsapi.org/v2/top-headlines?country=id&apiKey=9094ba21c74e472aabce0f3b7211d67a"));
+    final response = await http.get(Uri.parse("${_baseUrl}top-headlines?country=$_country&category=$_category&apiKey=$_apiKey"));
     if (response.statusCode == 200){
       return ArticlesResult.fromJson(json.decode(response.body));
     } else {
